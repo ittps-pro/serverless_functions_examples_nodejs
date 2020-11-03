bcd0bb73c2e7fd0e750d8b8a865254568290d06a
Max Vyaznikov <vyaznikov@selectel.ru>
2020-11-03 15:27:40 +0300

    Added hello.js (very simple example)

---
+++ hello.js
@@ -0,0 +1,11 @@
+
+module.exports.main = (event) => {
+  /**
+   * Это вызываемая функция.
+   * Она будет выполняться для каждого отдельного запроса.
+   *
+   * This is function to execute.
+   * It will be invoked for each request.
+   */
+  return `Hello, ${JSON.stringify(event)}!`
+}
