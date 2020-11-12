5b0070a2d19bfc97f00d49268054c0212d9779f5
Max Vyaznikov <vyaznikov@selectel.ru>
2020-11-12 14:40:41 +0300

    Made info_async_facade.js more readable

--- info_async_facade.js
+++ info_async_facade.js
@@ -11,56 +11,69 @@ const http = require('http');
  */
 module.exports.main = async ({lat, lon}) => {
   return {
-    weather_forecast: await get_weather_forecast(lat, lon),
+    current_weather: await get_current_weather(lat, lon),
     exchange_rates: await get_exchange_rates(),
   }
 }
 
 
 /**
- * Функция для получения прогноза погоды от сервиса OpenWeatherMap.
+ * Функция для получения текущей погоды от сервиса OpenWeatherMap.
  *
- * Function to get weather forecast by data from OpenWeatherMap.
+ * Function to get current weather by data from OpenWeatherMap.
  *
  * @param lat latitude
  * @param lon longitude
  * @returns {Promise<*>}
  */
-async function get_weather_forecast(lat, lon) {
-  let url = `${OPENWEATHERMAP_URL_BASE}?units=metric&lat=${lat}&lon=${lon}`
-    + `&appid=${OPENWEATHERMAP_API_KEY}`;
-  return _fetch_json(url)
+async function get_current_weather(lat, lon) {
+  return _fetch_json(`${OPENWEATHERMAP_URL_BASE}&lat=${lat}&lon=${lon}`)
     // Transform data.
     .then((data) => {
-      return typeof data['list'] !== 'undefined' ? data['list'] : {};
+      if (typeof data['main'] !== 'undefined') {
+        return {
+          ...data['main'],
+          wind: data['wind']
+        };
+      } else {
+        console.error("Can't transform data from OWM: ", data);
+        return {}
+      }
     });
 }
 // OpenWeatherMap API endpoint.
-// For more information see: https://openweathermap.org/forecast5#geo5
-OPENWEATHERMAP_URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
-OPENWEATHERMAP_API_KEY = process.env.OWM_API_KEY;
+// For more information see: https://openweathermap.org/current
+OPENWEATHERMAP_URL_BASE = "http://api.openweathermap.org/data/2.5/weather"
+  + `?appid=${process.env.OWM_API_KEY}&units=metric`;
 
 
 /**
- * Функция для получения текущих котировок мировых валют относительно
- * доллара США по данным OpenExchangeRates.
+ * Функция для получения текущих котировок мировых валют по данным OpenExchangeRates.
  *
- * Function get exchange rates based on USD by data from OpenExchangeRates.
+ * Function get exchange rates by data from OpenExchangeRates.
  *
  * @returns {Promise<*>}
  */
 async function get_exchange_rates() {
-  let url = `${OPENEXCHANGERATES_URL_BASE}?app_id=${OPENEXCHANGERATES_API_KEY}`;
-  return _fetch_json(url)
+  return _fetch_json(OPENEXCHANGERATES_URL)
     // Transform data.
     .then((data) => {
-      return typeof data['rates'] !== 'undefined' ? data['rates'] : {};
+      if (typeof data['rates'] !== 'undefined') {
+        // Filter currencies. Show only few.
+        return Object.fromEntries(Object.entries(data['rates'])
+          .filter((v) => {
+            return ['RUB', 'EUR', 'JPY', 'GBP'].indexOf(v[0]) >= 0
+          }))
+      } else {
+        console.error("Can't transform data from OXR: ", data);
+        return {}
+      }
     });
 }
 // OpenExchangeRates API endpoint.
 // For more information see: https://openexchangerates.org
-OPENEXCHANGERATES_URL_BASE = "http://openexchangerates.org/api/latest.json";
-OPENEXCHANGERATES_API_KEY = process.env.OXR_API_KEY;
+OPENEXCHANGERATES_URL = "http://openexchangerates.org/api/latest.json"
+  + `?app_id=${process.env.OXR_API_KEY}&base=USD`;
 
 
 /**
@@ -80,8 +93,7 @@ async function _fetch_json(url) {
         body.push(chunk);
       });
       res.on('end', () => {
-        body = Buffer.concat(body).toString();
-        resolve(JSON.parse(body));
+        resolve(JSON.parse(Buffer.concat(body).toString()));
       });
     }).on('error', (e) => {
       // Log errors and not throw it.
